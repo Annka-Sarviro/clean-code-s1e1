824d14b6cdcd100fe4c1015297597da91311b3f3
Ann Sarviro <annka_nuha@ai.ua>
2023-04-21 11:49:47 +0300

    refactor: add all teg to img to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement = function (taskString) {
   deleteButton.className = "button delete-button";
   deleteButtonImg.src = "./remove.svg";
   deleteButtonImg.className = "button-img";
+  deleteButtonImg.alt = "";
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
